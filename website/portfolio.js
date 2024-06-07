8dcb6ec6bcedbd10a0c55ad66390252c74dc17c6
ztifff <eduartefitzgerald@gmail.com>
2024-06-08 00:48:11 +0800

    g

---
+++ website/portfolio.js
@@ -0,0 +1,33 @@
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
+
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
+
+var sectionIndex = 0;
+
+leftArrow.addEventListener("click", function () {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 2;
+    slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+});
+rightArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 0;
+    slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+});
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("lightmode");
+    if (document.body.classList.contains("lightmode")) {
+        icon.src = "moon.png";
+    } else {
+        icon.src = "sun.png";
+    }
+}
